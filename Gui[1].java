8e81a2d73f85302e689f78802c2c9a807f8ded25
Pujam.Shyam <81807241+pujamshyam@users.noreply.github.com>
2022-12-14 16:39:16 +0530

    Add files via upload
    Number Guessing Game
    
    It is a short Java Project in this we have to guess the number which is randomly genereted by the computer.

---
+++ Gui[1].java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+/**
+ *
+ * @author shyam
+ */
+public class Gui {
+    public static void _centerFrame(JFrame frame){
+        Rectangle rec = frame.getBounds();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation((dim.width - rec.width)/2 , (dim.height - rec.height)/2);
+    }
+      
+    
+    public static void _integerOnly(KeyEvent evt)
+    {
+        char _character = evt.getKeyChar();
+        if(Character.isDigit(_character)
+                || _character == KeyEvent.VK_SPACE
+                 || _character == KeyEvent.VK_DELETE
+                 || _character == KeyEvent.VK_SLASH){}
+    else {
+            evt.consume();
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }
+    public static void _message(String message, int errorTye){
+        switch(errorTye){
+            case 0:
+                JOptionPane.showMessageDialog(null, message,"ERROR", JOptionPane.ERROR_MESSAGE);
+                break;
+            case 1: 
+                JOptionPane.showMessageDialog(null, message,"SUCCESS", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2: 
+                JOptionPane.showMessageDialog(null, message,"WARNING", JOptionPane.WARNING_MESSAGE);
+                break;
+            default:
+                break;
+        }
+    }
+
+private static String patFileBestScore = System.getProperty("user.dir") + "/src/GUI/bestscore.ini";
+public static int getCurrentBestScore(){
+    int bestScore;
+    String line, newLine = "";
+    try{
+        BufferedReader buff = new BufferedReader(new FileReader(patFileBestScore));
+       while ((line = buff.readLine()) != null){
+           newLine = line;
+       }
+        
+    }
+    catch(FileNotFoundException e){  
+    }   catch (IOException ex) {
+            Logger.getLogger(Gui.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    bestScore = Integer.parseInt(newLine.substring(newLine.indexOf("[") + 1, newLine.indexOf("]")));
+    return bestScore;
+}
+public static void setNewBestScore(String newContent){
+    try{
+        File fileToUpdate = new File(patFileBestScore);
+        try(FileWriter fileWrite = new FileWriter(fileToUpdate)){
+            fileWrite.write(newContent);
+            fileWrite.close();
+        }
+    }catch (Exception e){
+        
+    }
+}
+}
+    
+        
+
+
+
+    
+    
+    
+
